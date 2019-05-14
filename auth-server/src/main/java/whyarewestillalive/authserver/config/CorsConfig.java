52b40b74f9256f00b8273de6b9be721b93b80944
Hajbo <hajbodev@gmail.com>
2019-05-14 11:22:22 +0200

    Unnecessary comments removed

--- auth-server/src/main/java/whyarewestillalive/authserver/config/CorsConfig.java
+++ auth-server/src/main/java/whyarewestillalive/authserver/config/CorsConfig.java
@@ -22,10 +22,7 @@ public class CorsConfig {
         config.addAllowedMethod("*");
         source.registerCorsConfiguration("/**", config);
         FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
-
-        //IMPORTANT #2: I didn't stress enough the importance of this line in my original answer,
-        //but it's here where we tell Spring to load this filter at the right point in the chain
-        //(with an order of precedence higher than oauth2's filters)
+        
         bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
         return bean;
     }
