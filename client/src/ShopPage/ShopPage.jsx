843795a13dfedf167ad5a8af9fcc421c3b7f022f
Hajbo <hajbodev@gmail.com>
2019-05-17 23:50:41 +0200

    Code formatting

--- client/src/ShopPage/ShopPage.jsx
+++ client/src/ShopPage/ShopPage.jsx
@@ -20,80 +20,92 @@ class ShopPage extends React.Component {
         newState.data = [
             {
                 id_: 1,
-                name: 'alma',
+                name: "alma",
                 price: 100,
-                description: 'This is an appleasddasdssadappleasddasdssadappleasddasdssadappleasddasdssadappleasddasdssadappleasddasdssadappleasddasdssadappleasddasdssadappleasddasdssad'
+                description:
+                    "This is an appleasddasdssadappleasddasdssadappleasddasdssadappleasddasdssadappleasddasdssadappleasddasdssadappleasddasdssadappleasddasdssadappleasddasdssad"
             },
             {
                 id_: 2,
-                name: 'banana',
+                name: "banana",
                 price: 200,
-                description: 'This is a banana'
-            },
+                description: "This is a banana"
+            }
         ];
         this.setState(newState);
         //userService.getAll().then(users => this.setState({ users }));
     }
 
     render() {
-        const { data, currentUser} = this.state;
+        const { data, currentUser } = this.state;
         console.log(data);
         return (
             <div>
-                { data && (
-                 <ReactTable
-                    data={data}
-                    
-                    filterable
-                    defaultFilterMethod={(filter, row) =>
-                        String(row[filter.id]).includes(filter.value)}
-                    getTdProps={(state, rowInfo, column, instance) => {
-                        return {
-                        onClick: (e, handleOriginal) => {
-                            if (handleOriginal) {
-                                handleOriginal(rowInfo.index, currentUser);
+                {data && (
+                    <ReactTable
+                        data={data}
+                        filterable
+                        defaultFilterMethod={(filter, row) =>
+                            String(row[filter.id]).includes(filter.value)
+                        }
+                        getTdProps={(state, rowInfo, column, instance) => {
+                            return {
+                                onClick: (e, handleOriginal) => {
+                                    if (handleOriginal) {
+                                        handleOriginal(
+                                            rowInfo.index,
+                                            currentUser
+                                        );
+                                    }
+                                }
+                            };
+                        }}
+                        columns={[
+                            {
+                                Header: "Shop items",
+                                columns: [
+                                    {
+                                        Header: "#",
+                                        accessor: "id_",
+                                        width: 50,
+                                        filterable: false
+                                    },
+                                    {
+                                        Header: "Name",
+                                        accessor: "name",
+                                        maxWidth: 100
+                                    },
+                                    {
+                                        Header: "Short description",
+                                        accessor: "description",
+                                        width: 300,
+                                        maxWidth: 500
+                                    },
+                                    {
+                                        Header: "Price",
+                                        accessor: "price",
+                                        width: 100
+                                    },
+                                    {
+                                        Header: "",
+                                        accessor: "add",
+                                        filterable: false,
+                                        Cell: ({ row }) => (
+                                            <button
+                                                onClick={() =>
+                                                    addToCart(row, currentUser)
+                                                }
+                                            >
+                                                Add to cart
+                                            </button>
+                                        )
+                                    }
+                                ]
                             }
-                        }
-                        };
-                    }}
-                    columns={[
-                        {
-                            Header: "Shop items",
-                            columns: [
-                                {
-                                    Header: "#",
-                                    accessor: "id_",
-                                    width: 50,
-                                    filterable: false,
-                                },
-                                {
-                                    Header: "Name",
-                                    accessor: "name",
-                                    maxWidth: 100
-                                },
-                                {
-                                    Header: "Short description",
-                                    accessor: "description",
-                                    width: 300,
-                                    maxWidth: 500
-                                },
-                                {
-                                    Header: "Price",
-                                    accessor: "price",
-                                    width: 100
-                                },
-                                {
-                                    Header: "",
-                                    accessor: "add",
-                                    filterable: false,
-                                    Cell: ({ row })  => (<button onClick={() => addToCart(row, currentUser)}>Add to cart</button>)
-                                },
-                            ]
-                        }
-                    ]}
-                    defaultPageSize={10}
-                    className="-striped -highlight"
-                />
+                        ]}
+                        defaultPageSize={10}
+                        className="-striped -highlight"
+                    />
                 )}
             </div>
         );
