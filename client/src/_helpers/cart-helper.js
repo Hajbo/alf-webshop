0e2f425df76fab12158be33325094a533d111e84
Hajbo <hajbodev@gmail.com>
2019-05-19 14:31:06 +0200

    Cart with mocked adata

--- client/src/_helpers/cart-helper.js
+++ client/src/_helpers/cart-helper.js
@@ -1,6 +1,14 @@
 function addToCart(row, user) {
     console.log(row);
     console.log(user);
-}
+};
 
-export { addToCart };
+function checkout(user) {
+    console.log('CHECKOUT');
+};
+
+function removeItemFromCart(row, user) {
+    console.log('REMOVE ITEM FROM CART');
+};
+
+export { addToCart, checkout, removeItemFromCart };
